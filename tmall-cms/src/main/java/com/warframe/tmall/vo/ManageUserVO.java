73aa7992cb8824eb08474fea84718cd21b1f7242
warframeprimer <yaojiabin@ysten.com>
2018-04-24 21:17:23 +0800

    【add】 R.I.P BB

---
+++ tmall-cms/src/main/java/com/warframe/tmall/vo/ManageUserVO.java
@@ -0,0 +1,30 @@
+package com.warframe.tmall.vo;
+
+
+
+
+import com.warframe.tmall.domain.TbManageUser;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 后台用户展示VO
+ *
+ * @author xubin.
+ * @create 2017-02-11 下午3:57
+ */
+
+
+public class ManageUserVO extends TbManageUser {
+
+    private String joinus;
+
+    public String getJoinus() {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy MM");
+
+        return format.format(this.getCreated()).toString() + "加入公司";
+    }
+
+
+}
