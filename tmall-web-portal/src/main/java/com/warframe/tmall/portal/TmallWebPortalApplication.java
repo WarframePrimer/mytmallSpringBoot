eef0dfe8a02d17e812ccd895574ff95b53239535
warframeprimer <yaojiabin@ysten.com>
2018-04-26 18:08:09 +0800

    【add】 R.I.P BB3

--- tmall-web-portal/src/main/java/com/warframe/tmall/portal/TmallWebPortalApplication.java
+++ tmall-web-portal/src/main/java/com/warframe/tmall/portal/TmallWebPortalApplication.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ServletComponentScan(basePackages = { "com.warframe.tmall"})
 @ComponentScan(basePackages = { "com.warframe.tmall" })
-@MapperScan(basePackages={"com.warframe.tmall.repository.*.mapper"})
+@MapperScan(basePackages={"com.warframe.tmall.repository"})
 public class TmallWebPortalApplication extends SpringBootServletInitializer {
 
     @Override
